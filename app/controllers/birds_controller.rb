aab0c73543d5b7c603c333fb1342260c4fbf8bcc
Kyle Imhoff <imhoff.kyle@gmail.com>
2021-01-01 15:42:32 +0000

    Automatically backed up by Learn

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,7 +1,6 @@
 class BirdsController < ApplicationController
   def index
     @birds = Bird.all
-    
-     render plain: "Hello #{@birds[3].name}"
+    render json: { birds: @birds, messages: ['Hello Birds', 'Goodbye Birds'] }
   end
 end
